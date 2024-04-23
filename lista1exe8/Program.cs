e431614d06e37a55c49107bdc27a2539f93deaad
User <User@DESKTOP-G0A3IIV>
2024-04-23 01:18:52 -0300

    Adicionar arquivos de projeto.

---
+++ lista1exe8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exe8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("conversor de temperatura de Celsius para Fahrenheit");
+            Console.WriteLine("Digite a temperatura em graus Celsius:");
+
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine($" A temperatura em Fahrenheit é: {fahrenheit:F2} °F");
+        }
+    }
+}
